a7e5f231d5a44b910d1fee95f690577c03170311
timothy.goric@gmail.com <timothy.goric@gmail.com@acc62554-7407-629c-8548-9586a19966d8>
2010-07-06 02:54:37 +0000

    Adding files I missed before

---
+++ SemanticAnalysis/TypeName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SemanticAnalysis
+{
+    public class TypeName : CFlatType
+    {
+        public CFlatType Base {get;set;}
+        public TypeName (CFlatType bse)
+        {
+            Base = bse;
+        }
+    }
+}
