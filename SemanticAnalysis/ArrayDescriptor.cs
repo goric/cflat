8cfbd002b739752f0c507d0023fbc0e5d17fed22
timothy.goric@gmail.com <timothy.goric@gmail.com@acc62554-7407-629c-8548-9586a19966d8>
2010-08-01 20:06:08 +0000

    Adding ArrayDescriptor

---
+++ SemanticAnalysis/ArrayDescriptor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SemanticAnalysis
+{
+    public class ArrayDescriptor : Descriptor
+    {
+        public CFlatType Type { get; set; }
+        public CFlatType BaseType { get; set; }
+        public String Name { get; private set; }
+        public int LowerBound { get; private set; }
+        public int UpperBound { get; private set; }
+
+        public ArrayDescriptor (CFlatType type, CFlatType baseType, string name, int lower, int upper)
+            : base(type)
+        {
+            Type = type;
+            BaseType = baseType;
+            Name = name;
+            LowerBound = lower;
+            UpperBound = upper;
+        }
+    }
+}
